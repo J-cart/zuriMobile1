d72e6fd340f6ddb128fefcdf64cb5d72ed130426
Joseph <j.t.cartoonist@gmail.com>
2021-05-10 22:38:10 +0100

    ...

--- app/src/main/java/com/android/listlogin/MainActivity.kt
+++ app/src/main/java/com/android/listlogin/MainActivity.kt
@@ -3,7 +3,9 @@ package com.android.listlogin
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.google.android.material.button.MaterialButton
+import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -14,11 +16,22 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun signIn() {
+        val email = findViewById<TextInputEditText>(R.id.edit_user)
+        val passcode = findViewById<TextInputEditText>(R.id.edit_passcode)
         val login = findViewById<MaterialButton>(R.id.Login_button)
 
         login.setOnClickListener(){
-            val intent = Intent(this, DetailActivity::class.java)
-            startActivity(intent)
+            if(email.text.toString().isNullOrBlank() || passcode.text.toString().isNullOrBlank()){
+                Toast.makeText(this, "Input Email or Password", Toast.LENGTH_SHORT).show()
+            } else{
+                if(email.text.toString() == "man@gmail.com" && passcode.text.toString() == "12345"){
+                    val intent = Intent(this, DetailActivity::class.java)
+                    startActivity(intent)
+                } else{
+                    Toast.makeText(this, "Wrong Email or Password", Toast.LENGTH_SHORT).show()
+                }
+            }
+
         }
     }
 }
