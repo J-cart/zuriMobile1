82c77e077e409abc71c16fd227a78daf706a7b04
Joseph <j.t.cartoonist@gmail.com>
2021-05-10 21:44:20 +0100

    ...

--- app/src/main/java/com/android/listlogin/DetailActivity.kt
+++ app/src/main/java/com/android/listlogin/DetailActivity.kt
@@ -2,10 +2,27 @@ package com.android.listlogin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.listlogin.databinding.ActivityDetailBinding
+import layout.DummyData
 
 class DetailActivity : AppCompatActivity() {
+
+    var binding: ActivityDetailBinding? = null
+    var adapter: FactAdapter? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_detail)
+        binding = ActivityDetailBinding.inflate(layoutInflater)
+        setContentView(binding?.root)
+        setUpFacts()
+    }
+
+    private fun setUpFacts(){
+        adapter = FactAdapter(this, DummyData.funFacts)
+        binding?.factListView?.adapter
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        binding = null
     }
 }
